fe1e42f8670cf9d180cf5f98f0561dfa84ed762d
Edsel Caprice <caprice@mail.usf.edu>
2018-05-21 02:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function total() {
 
 function removeFromCart(item) {
  if (cart.indexOf(item) === -1) {
-   return "That item is not in your cart."
+    console.log("That item is not in your cart.")
  } else {
   for (let i = 0; i<cart.length;i++) {
     if (cart[i].itemName === item) {
