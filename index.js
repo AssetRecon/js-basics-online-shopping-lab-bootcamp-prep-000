aea44d48a63c5053b5028e441ded33728cfe5972
Edsel Caprice <caprice@mail.usf.edu>
2018-05-21 03:25:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    
+    cart2 = cart
+    sum = total()
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;  
 }
 }
